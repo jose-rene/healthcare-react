037713b7f570b227a4eac5be49aac0ae42ed257b
jose-rene <alvarezsoftwaredevcom@gmail.com>
2020-08-18 15:07:15 -0400

    fix error with redux dev tools

--- web/src/store.js
+++ web/src/store.js
@@ -1,10 +1,14 @@
-import {applyMiddleware, compose, createStore} from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const enhancer = compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+/* eslint-disable no-underscore-dangle */
+const enhancer = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+    : []),
+];
+/* eslint-enable no-underscore-dangle */
 
-export default createStore(rootReducer, enhancer);
+export default createStore(rootReducer, compose(...enhancer));
