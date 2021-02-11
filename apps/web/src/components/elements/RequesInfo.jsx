52ca139d5a4d975a7063d4e7c1cfa336cb576adb
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-02-11 10:14:04 -0500

    Php 8 compat, protected error page route

--- apps/web/src/components/elements/RequesInfo.jsx
+++ apps/web/src/components/elements/RequesInfo.jsx
@@ -9,7 +9,7 @@ const Info = () => {
     });
 
     useEffect(() => {
-        // fireRequest();
+        fireRequest();
     }, []);
 
     return (
--- apps/web/src/pages/Errors/401.jsx
+++ apps/web/src/pages/Errors/401.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import PageAlert from "../../components/elements/PageAlert";
 import PageLayout from "../../layouts/PageLayout";
 
 const Error401 = () => {
     return (
         <PageLayout>
-            <header className="App-header">
-                <h3 className="text-center text-dark">
-                    Access denied. It does not seem like you have access to view
-                    the requested page.
-                </h3>
-            </header>
+            <Container className="container mt-5">
+                <div className="row">
+                    <div className="col-md-10 col-md-offset-2">
+                        <PageAlert variant="danger">
+                            Access denied. You do not have permission to the
+                            requested page.
+                        </PageAlert>
+                    </div>
+                </div>
+            </Container>
         </PageLayout>
     );
 };
