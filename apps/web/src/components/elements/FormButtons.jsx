ca96f96680d6b123c19eed165cd73c7979569eab
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-03-11 08:28:54 -0700

    fix FormButtons

--- apps/web/src/components/elements/FormButtons.jsx
+++ apps/web/src/components/elements/FormButtons.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import Button from "../inputs/Button";
 
-const FormButtons = ({ submitLabel = "Submit", onCancel = () => {} }) => {
+const FormButtons = ({
+    submitLabel = "Submit",
+    cancelLabel = "Cancel",
+    onCancel,
+}) => {
     return (
         <div className="form-row">
+            <div className="col-md-5">
+                <Button className="btn btn-block" type="Submit">
+                    {submitLabel}
+                </Button>
+            </div>
+
             <div className="col-md-5">
                 <Button
                     className="btn btn-block"
                     variant="secondary"
                     onClick={() => onCancel()}
                 >
-                    Cancel
-                </Button>
-            </div>
-
-            <div className="col-md-5">
-                <Button className="btn btn-block" type="Submit">
-                    {submitLabel}
+                    {cancelLabel}
                 </Button>
             </div>
         </div>
