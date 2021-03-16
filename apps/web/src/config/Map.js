bd17d0e472eed4f1aca5310eca110a4bdc69b276
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-03-16 17:37:17 +0000

    Integrate google map api

---
+++ apps/web/src/config/Map.js
@@ -0,0 +1,7 @@
+const {
+    MAP_API_KEY = "AIzaSyCYHqP5sGELeYR6eTPunrWgz1p6ctQ2DHs",
+    MAP_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json",
+} = process.env;
+
+export const API_KEY = MAP_API_KEY;
+export const BASE_URL = MAP_BASE_URL;
