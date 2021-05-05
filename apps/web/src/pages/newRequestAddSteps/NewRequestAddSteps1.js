0645bc3783907dd51a1b15d1078f40d2abcad34f
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-05-05 07:13:51 -0600

    new request add form validation

--- apps/web/src/pages/newRequestAddSteps/NewRequestAddSteps1.js
+++ apps/web/src/pages/newRequestAddSteps/NewRequestAddSteps1.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { isEmpty } from "lodash";
 
 import Switch from "../../components/inputs/Switch";
