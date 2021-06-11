01e71649f184d6f945670dcbbac5919d261bfb70
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-06-10 20:45:52 -0600

    add Notes Tab

---
+++ apps/web/src/pages/Admin/Companies/Tabs/TabNotes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Textarea from "../../../../components/inputs/Textarea";
+
+const TabNotes = () => {
+    return (
+        <div className="col-lg-7 col-md-12 pl-0">
+            <div className="white-box white-box-small">
+                <Textarea
+                    className="form-control custom-textarea-input"
+                    value="Activity Update"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default TabNotes;
