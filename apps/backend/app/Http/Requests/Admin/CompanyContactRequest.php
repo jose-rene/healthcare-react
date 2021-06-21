28e95a2fb0d43737efbd8b0d4de12d8413e932d3
jose-rene <alvarezsoftwaredevcom@gmail.com>
2021-06-21 17:38:52 -0400

    update contact request validation

--- apps/backend/app/Http/Requests/Admin/CompanyContactRequest.php
+++ apps/backend/app/Http/Requests/Admin/CompanyContactRequest.php
@@ -24,8 +24,24 @@ class CompanyContactRequest extends FormRequest
     public function rules()
     {
         return [
-            'contacts'   => ['bail', 'required', 'min:1'],
-            'contacts.*' => ['bail', 'required'],
+            'contacts'         => ['bail', 'required', 'min:1'],
+            'contacts.*'       => ['bail', 'required'],
+            'contacts.*.type'  => ['bail', 'required'],
+            'contacts.*.value' => ['bail', 'required'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'contacts.*.required'       => 'Contact information is required',
+            'contacts.*.type.required'  => 'A contact type is required',
+            'contacts.*.value.required' => 'An email or phone number is required',
         ];
     }
 }
